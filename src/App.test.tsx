f8d7d2eeeeee68a557ea31049e5e735564548acf
YTXEternal <3781216085@qq.com>
2026-03-19 02:37:23 +0800

    build: 集成基本环境

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders get started text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Get started/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,42 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import UnoCSS from 'unocss/vite'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
   plugins: [
     UnoCSS(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    dts({
+      insertTypesEntry: true,
+      include: ['src/components/**/*.ts', 'src/components/**/*.tsx', 'src/index.ts'],
+    })
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'UxTableReact',
+      fileName: (format) => `ux-table-react.${format}.js`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime'
+        }
+      }
+    }
+  }
 })
