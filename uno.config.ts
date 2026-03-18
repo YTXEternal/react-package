afeb2ac369cdf36cf22779f82f5f8157906076da
YTXEternal <3781216085@qq.com>
2026-03-19 01:36:38 +0800

    build: unocss预设常用css

--- uno.config.ts
+++ uno.config.ts
@@ -6,4 +6,24 @@ export default defineConfig({
     presetAttributify(),
     presetIcons(),
   ],
+  shortcuts: [
+    // 宽高全屏/全铺满
+    ['wh-full', 'w-full h-full'],
+    ['wh-screen', 'w-screen h-screen'],
+    
+    // Flex 布局
+    ['f-c', 'flex justify-center items-center'],
+    ['f-c-c', 'flex flex-col justify-center items-center'],
+    ['f-between', 'flex justify-between items-center'],
+    ['f-around', 'flex justify-around items-center'],
+    ['f-start', 'flex justify-start items-center'],
+    ['f-end', 'flex justify-end items-center'],
+    
+    // 定位
+    ['absolute-center', 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'],
+    ['fixed-center', 'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'],
+    
+    // 文本
+    ['text-ellipsis', 'truncate'],
+  ],
 })
